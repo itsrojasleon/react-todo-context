286ff607bc4a98c925e7c9aafab3e62dc3de7942
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-06-13 19:08:36 -0500

    Remove console.log

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -13,7 +13,6 @@ import { DispatchContext } from '../../contexts/TodosContext';
 function Todo({ id, task, completed }) {
   const dispatch = useContext(DispatchContext);
   const [isEditing, toggle] = useToggleState(false);
-  console.log(id);
   return (
     <ListItem style={{ height: '64px' }}>
       {isEditing ? (
